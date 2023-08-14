7c03ebd688c3ed53d271620dabe4169d1da59768
Bilal Softic <bilalsoftic99@gmail.com>
2023-08-14 16:53:40 +0200

    IzzoTextInput component

--- src/Registration.jsx
+++ src/Registration.jsx
@@ -1,7 +1,8 @@
-import { StyleSheet, Text, TextInput, View, Button } from "react-native";
-import { useState, useRef } from "react";
-import { initialUserState } from "../config/registrationConfig";
-import { registerUser } from "../izo-api";
+import { StyleSheet, Text, ScrollView, Button } from 'react-native';
+import { useState, useRef } from 'react';
+import { initialUserState } from '../config/registrationConfig';
+import { registerUser } from '../izo-api';
+import IzzoTextInput from './components/IzzoTextInput';
 
 export default function Registration() {
   const [user, setUser] = useState({ ...initialUserState });
@@ -11,7 +12,7 @@ export default function Registration() {
   };
 
   const ref_lastName = useRef(),
-    ref_username = useRef(),
+    ref_userName = useRef(),
     ref_email = useRef(),
     ref_password = useRef(),
     ref_password_confirmation = useRef();
@@ -24,101 +25,78 @@ export default function Registration() {
   };
 
   return (
-    <View style={styles.container}>
-      <Text>Sign Up</Text>
-      <View>
-        <Text>First Name</Text>
-        <TextInput
-          placeholder="First Name"
-          onChangeText={(e) => handleChange("first_name", e)}
-          value={user.first_name}
-          onSubmitEditing={() => {
-            ref_lastName.current.focus();
-          }}
-          blurOnSubmit={false}
-          returnKeyType="next"
-        />
-      </View>
-      <View>
-        <Text>Last Name</Text>
-        <TextInput
-          placeholder="Last Name"
-          ref={ref_lastName}
-          value={user.last_name}
-          onChangeText={(e) => handleChange("last_name", e)}
-          onSubmitEditing={() => {
-            ref_username.current.focus();
-          }}
-          blurOnSubmit={false}
-          returnKeyType="next"
-        />
-      </View>
-      <View>
-        <Text>UserName</Text>
-        <TextInput
-          placeholder="Display Name"
-          ref={ref_username}
-          value={user.username}
-          onChangeText={(e) => handleChange("username", e)}
-          onSubmitEditing={() => {
-            ref_email.current.focus();
-          }}
-          blurOnSubmit={false}
-          returnKeyType="next"
-        />
-      </View>
-      <View>
-        <Text>Email</Text>
-        <TextInput
-          placeholder="Email"
-          ref={ref_email}
-          value={user.email}
-          onChangeText={(e) => handleChange("email", e)}
-          onSubmitEditing={() => {
-            ref_password.current.focus();
-          }}
-          blurOnSubmit={false}
-          returnKeyType="next"
-        />
-      </View>
-      <View>
-        <Text>Password</Text>
-        <TextInput
-          placeholder="Password"
-          ref={ref_password}
-          value={user.password}
-          onChangeText={(e) => handleChange("password", e)}
-          onSubmitEditing={() => {
-            ref_password_confirmation.current.focus();
-          }}
-          blurOnSubmit={false}
-          returnKeyType="next"
-        />
-      </View>
-      <View>
-        <Text>Password</Text>
-        <TextInput
-          placeholder="Confirm Password"
-          ref={ref_password_confirmation}
-          value={user.password_confirmation}
-          onChangeText={(e) => handleChange("password_confirmation", e)}
-        />
-      </View>
+    <ScrollView
+      automaticallyAdjustKeyboardInsets={true}
+      style={styles.container}
+    >
+      <Text style={styles.izzoHeader}>Sign Up</Text>
+      <IzzoTextInput
+        inputName="First Name"
+        handleChangeKey="first_name"
+        nextRef={ref_lastName}
+        user={user}
+        handleChange={handleChange}
+      />
+      <IzzoTextInput
+        inputName="Last Name"
+        handleChangeKey="last_name"
+        ref={ref_lastName}
+        nextRef={ref_userName}
+        user={user}
+        handleChange={handleChange}
+      />
+      <IzzoTextInput
+        inputName="User Name"
+        handleChangeKey="username"
+        ref={ref_userName}
+        nextRef={ref_email}
+        user={user}
+        handleChange={handleChange}
+      />
+      <IzzoTextInput
+        inputName="E-mail"
+        handleChangeKey="email"
+        ref={ref_email}
+        nextRef={ref_password}
+        user={user}
+        handleChange={handleChange}
+      />
+      <IzzoTextInput
+        inputName="Password"
+        handleChangeKey="password"
+        ref={ref_password}
+        nextRef={ref_password_confirmation}
+        user={user}
+        handleChange={handleChange}
+      />
+      <IzzoTextInput
+        inputName="Confirm Password"
+        handleChangeKey="password_confirmation"
+        ref={ref_password_confirmation}
+        user={user}
+        handleChange={handleChange}
+      />
+
       <Button
         onPress={submitRegistration}
         title="Submit"
         color="#841584"
         accessibilityLabel="Learn more about this purple button"
       />
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    marginTop: 40,
-    marginLeft: 5,
+    backgroundColor: '#0C6A9F',
+    marginTop: 30,
+  },
+  izzoHeader: {
+    color: '#FFFFFF',
+    fontSize: 40,
+    alignSelf: 'center',
+    marginVertical: 25,
   },
 });
---
+++ src/components/IzzoTextInput.jsx
@@ -0,0 +1,53 @@
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { forwardRef } from 'react';
+
+const IzzoTextInput = forwardRef(
+  (Input = (props, ref) => {
+    const { inputName, handleChangeKey, nextRef, user, handleChange } = props;
+    return (
+      <View style={styles.izzoInputView}>
+        {console.log(nextRef)}
+        <Text style={styles.izzoInputLabel}>{inputName}</Text>
+        <TextInput
+          style={styles.izzoTextInput}
+          placeholder={inputName}
+          ref={ref}
+          onChangeText={(e) => handleChange(handleChangeKey, e)}
+          value={user[handleChangeKey]}
+          onSubmitEditing={() => {
+            nextRef ? nextRef.current.focus() : null;
+          }}
+          blurOnSubmit={false}
+          returnKeyType={nextRef ? 'next' : 'submit'}
+        />
+      </View>
+    );
+  })
+);
+const styles = StyleSheet.create({
+  izzoInputView: {
+    backgroundColor: '#0C6A9F',
+    marginVertical: 10,
+    gap: 3,
+  },
+  izzoTextInput: {
+    backgroundColor: '#FFFFFF',
+    fontSize: 17,
+    marginHorizontal: 15,
+    height: 50,
+    borderRadius: 8,
+    paddingLeft: 15,
+  },
+  izzoInputLabel: {
+    color: '#FFFFFF',
+    paddingLeft: 27,
+  },
+  izzoHeader: {
+    color: '#FFFFFF',
+    fontSize: 40,
+    alignSelf: 'center',
+    marginVertical: 25,
+  },
+});
+
+export default IzzoTextInput;
